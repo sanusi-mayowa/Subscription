57060e992048fabeca0ba2922bb0a2f2866e47db
Sanusi Mayowa <sanusifaruq007@gmail.com>
2024-02-15 12:16:47 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,17 @@
+// function validateEmail() {
+//   var emailInput = document.getElementById("email");
+//   var errorMessage = document.getElementById("error-message");
+
+//   // Regular expression for a simple email validation
+//   var emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+
+//   if (emailRegex.test(emailInput.value)) {
+//     // Valid email
+//     errorMessage.style.display = "none";
+//     emailInput.style.border = "1px solid #ccc";
+//   } else {
+//     // Invalid email
+//     errorMessage.style.display = "block";
+//     emailInput.style.border = "1px solid red";
+//   }
+// }
